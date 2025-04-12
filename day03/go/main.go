9a75032ecdf3579369f85366d1f7b3d0291f3542
k4y0x13 <k4y0x13@gmail.com>
2025-04-12 16:15:35 +0600

    wip day3 golang

--- day03/go/main.go
+++ day03/go/main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
+type Point struct {
+	X, Y int
+}
+
 func SolvePart2(input string) int {
+	// process the input
+	directions := strings.Split(input, "")
+	var santa_directions []string
+	var robot_directions []string
+
+	for index, direction := range directions {
+		if index%2 == 0 {
+			santa_directions = append(santa_directions, direction)
+		} else {
+			robot_directions = append(robot_directions, direction)
+		}
+	}
+
+	fmt.Println("santa directions:", santa_directions)
+	fmt.Println("robot directions:", robot_directions)
 	return 0
 }
 
