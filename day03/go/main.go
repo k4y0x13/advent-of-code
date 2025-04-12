a009708f498b255355798f59966bf26c3d442b36
k4y0x13 <k4y0x13@gmail.com>
2025-04-12 16:27:41 +0600

    electicity went out part1 solution lost. had to rewrite the frekin part2 solution again

--- day03/go/main.go
+++ day03/go/main.go
@@ -26,7 +26,74 @@ func SolvePart2(input string) int {
 
 	fmt.Println("santa directions:", santa_directions)
 	fmt.Println("robot directions:", robot_directions)
-	return 0
+
+	// the solutions
+	pointSet := make(map[Point]bool)
+	pointSet[Point{0, 0}] = true
+	// santa goes and delivers
+	x_santa := 0
+	y_santa := 0
+	for _, direction := range santa_directions {
+		if direction == "^" {
+			y_santa += 1
+			if !pointSet[Point{x_santa, y_santa}] {
+				// doesn't exist
+				pointSet[Point{x_santa, y_santa}] = true
+			}
+		} else if direction == "v" {
+			y_santa -= 1
+			if !pointSet[Point{x_santa, y_santa}] {
+				// doesn't exist
+				pointSet[Point{x_santa, y_santa}] = true
+			}
+		} else if direction == ">" {
+			x_santa += 1
+			if !pointSet[Point{x_santa, y_santa}] {
+				// doesn't exist
+				pointSet[Point{x_santa, y_santa}] = true
+			}
+		} else if direction == "<" {
+			x_santa -= 1
+			if !pointSet[Point{x_santa, y_santa}] {
+				// doesn't exist
+				pointSet[Point{x_santa, y_santa}] = true
+			}
+		}
+	}
+
+	// robot goes to deliver
+	x_robot := 0
+	y_robot := 0
+	for _, direction := range robot_directions {
+		if direction == "^" {
+			y_robot += 1
+			if !pointSet[Point{x_robot, y_robot}] {
+				// doesn't exist
+				pointSet[Point{x_robot, y_robot}] = true
+			}
+		} else if direction == "v" {
+			y_robot -= 1
+			if !pointSet[Point{x_robot, y_robot}] {
+				// doesn't exist
+				pointSet[Point{x_robot, y_robot}] = true
+			}
+		} else if direction == ">" {
+			x_robot += 1
+			if !pointSet[Point{x_robot, y_robot}] {
+				// doesn't exist
+				pointSet[Point{x_robot, y_robot}] = true
+			}
+		} else if direction == "<" {
+			x_robot -= 1
+			if !pointSet[Point{x_robot, y_robot}] {
+				// doesn't exist
+				pointSet[Point{x_robot, y_robot}] = true
+			}
+		}
+	}
+
+	total_visited := len(pointSet)
+	return total_visited
 }
 
 func main() {
