3fd0b832db49da390b49f8b7e550c4e756021606
k4y0x13 <k4y0x13@gmail.com>
2025-12-04 22:19:26 +0600

    day 2 wip i still don't knkow

--- day2/solution.go
+++ day2/solution.go
@@ -18,9 +18,12 @@ func part1(input string) int {
 	// 2. Separate each range with - for start and end
 	for _, range_value := range ranges {
 		start_end := strings.Split(range_value, "-")
+		if len(start_end) < 2 {
+			continue
+		}
 		start_str := start_end[0]
 		end_str := start_end[1]
-		fmt.Println("\nrange:", range_value, "\nstart:", start_str, "\nend:", end_str)
+		// fmt.Println("\nrange:", range_value, "\nstart:", start_str, "\nend:", end_str)
 		// 3. loop through start and end, inclusive (int)
 		start_int, err := strconv.Atoi(start_str)
 		if err != nil {
@@ -42,7 +45,7 @@ func part1(input string) int {
 			// Else, check for repeats
 			mid_index := len(i_str) / 2
 			// fmt.Println("mid index: ", mid_index)
-			fmt.Println("Comparing:", i_str[:mid_index], "with: ", i_str[mid_index:])
+			// fmt.Println("Comparing:", i_str[:mid_index], "with: ", i_str[mid_index:])
 			if i_str[:mid_index] == i_str[mid_index:] {
 				// fmt.Println("Invalid id found:", i)
 				total_sum += i
@@ -57,12 +60,86 @@ func part1(input string) int {
 	// 4.3 if even, divide into two and check if it's repeating
 	// 4.4 If it's repeating, add to the sum value
 
-	fmt.Println("Total sum:", total_sum)
+	// fmt.Println("Total sum:", total_sum)
 	return total_sum
 }
 
+// the problem changes to n-repeated subsequence
 func part2(input string) int {
-	return 2
+	total_sum := 0
+
+	ranges := strings.Split(input, ",")
+	for _, value := range ranges {
+		each_range := strings.Split(value, "-")
+		if len(each_range) < 2 {
+			continue
+		}
+
+		start_str := each_range[0]
+		end_str := each_range[1]
+		fmt.Printf("converting start str %v and end str %v", start_str, end_str)
+		start_int, err := strconv.Atoi(start_str)
+		if err != nil {
+			log.Fatal("cannot convert start str to int")
+		}
+		end_int, err := strconv.Atoi(end_str)
+		if err != nil {
+			log.Fatal("cannot convert end str to int")
+		}
+
+		// for each number in the range
+	each_number_loop:
+		for each_number := start_int; each_number <= end_int; each_number++ {
+			each_number_str := strconv.Itoa(each_number)
+			// 1. check if it's a multiple
+			// 0 till length of str half
+
+			for win_size := 1; win_size <= (len(each_number_str) / 2); win_size++ {
+				// fmt.Printf("\nlength of number str: %v\n", len(each_number_str))
+				if len(each_number_str)%win_size != 0 { // if it's not a multiple
+					// not, so continue
+					continue
+				}
+				// then it's divisible
+				// add every subdivisions into set
+				// create an empty set and add each substring the divisions into the set
+				set := make(map[string]struct{}) // map of string to struct?
+				var void struct{}
+
+				// adding items: set["apple"] = void
+				// checking for existence: _, exists = set["grape"])
+				// the exist variable would be either true or false
+				// delete: delete(set, "banana")
+				// print: for element:= range set{fmt.print(element)}
+
+				// divide string into substrings with the given win_size and add to them
+				sub_div_start_index := 0
+				fmt.Println("\nwin size: ", win_size)
+				fmt.Println("each number in consideration: ", each_number_str)
+				for sub_div_index := win_size; sub_div_index < len(each_number_str)+1; sub_div_index += win_size {
+					fmt.Printf("sub_div  start: %v, sub_div end: %v\n", sub_div_start_index, sub_div_index)
+					set[each_number_str[sub_div_start_index:sub_div_index]] = void
+					sub_div_str := each_number_str[sub_div_start_index:sub_div_index]
+					fmt.Println("subdivision: ", sub_div_str)
+					sub_div_start_index += win_size - 1
+					fmt.Printf("length of set: %v\n", len(set))
+					fmt.Println("set: ", set)
+					if len(set) == 1 {
+						fmt.Printf("\n=====\nadding %v to total sum %v\n=====\n", each_number, total_sum)
+						total_sum += each_number
+						break each_number_loop
+					}
+				}
+
+			}
+
+		}
+		//
+	}
+
+	// fmt.Println("Total sum:", total_sum)
+	fmt.Println("")
+	return total_sum
 }
 
 func readInput(filename string) (string, error) {
